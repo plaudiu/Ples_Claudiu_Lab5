687d8dc0c1599eae224322e8bb819e39188ab103
Claudiu Ples <cples@yahoo.com>
2021-11-08 13:21:57 +0200

    Laboratorul 6

--- Ples_Claudiu_Lab5/MainWindow.xaml.cs
+++ Ples_Claudiu_Lab5/MainWindow.xaml.cs
@@ -12,28 +12,190 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AutoLotModel;
+using System.Data.Entity;
 
 namespace Ples_Claudiu_Lab5
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+
     public partial class MainWindow : Window
     {
+        ActionState action = ActionState.Nothing;
+        AutoLotEntitiesModel ctx = new AutoLotEntitiesModel();
+        CollectionViewSource customerVSource;
+
+        AutoLotEntitiesModel itx = new AutoLotEntitiesModel();
+        CollectionViewSource inventoryVsource;
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            customerVSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            customerVSource.Source = ctx.Customers.Local;
+            ctx.Customers.Load();
             System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // customerViewSource.Source = [generic data source]
             System.Windows.Data.CollectionViewSource inventoryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("inventoryViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // inventoryViewSource.Source = [generic data source]
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.New;
+        }
+        private void btnEditO_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Edit;
+        }
+        private void btnDeleteO_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Delete;
+        }
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            customerVSource.View.MoveCurrentToNext();
+        }
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            customerVSource.View.MoveCurrentToPrevious();
+        }
+        private void btnNextInv_Click(object sender, RoutedEventArgs e)
+        {
+            inventoryVSource.View.MoveCurrentToNext();
+        }
+        private void btnPrevInv_Click(object sender, RoutedEventArgs e)
+        {
+            inventoryVSource.View.MoveCurrentToPrevious();
+        }
+        private void SaveCustomers()
+        {
+            Customer customer = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    //instantiem Customer entity
+                    customer = new Customer()
+                    {
+                        FirstName = firstNameTextBox.Text.Trim(),
+                        LastName = lastNameTextBox.Text.Trim()
+                    };
+                    //adaugam entitatea nou creata in context
+                    ctx.Customers.Add(customer);
+                    customerVSource.View.Refresh();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                //using System.Data;
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+           if (action == ActionState.Edit)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    customer.FirstName = firstNameTextBox.Text.Trim();
+                    customer.LastName = lastNameTextBox.Text.Trim();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    ctx.Customers.Remove(customer);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerVSource.View.Refresh();
+            }
+        }
+        private void SaveInventory()
+        {
+            Inventory inventory = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    //instantiem Customer entity
+                    inventory = new Inventory()
+                    {
+                        Color = colorTextBox.Text.Trim(),
+                        Make = makeTextBox.Text.Trim()
+                    };
+                    //adaugam entitatea nou creata in context
+                    itx.Inventory.Add(inventory);
+                    inventoryVSource.View.Refresh();
+                    //salvam modificarile
+                    itx.SaveChanges();
+                }
+                //using System.Data;
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+           if (action == ActionState.Edit)
+            {
+                try
+                {
+                    inventory = (Inventory)inventoryDataGrid.SelectedItem;
+                    inventory.Color = colorTextBox.Text.Trim();
+                    inventory.Make = makeTextBox.Text.Trim();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    inventory = (Inventory)customerDataGrid.SelectedItem;
+                    itx.Inventory.Remove(inventory);
+                    itx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                inventoryVSource.View.Refresh();
+            }
+
         }
     }
 }
